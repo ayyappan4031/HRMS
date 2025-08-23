842175f4cd8f9307305e333af64c4be1e931e34f
ayyappan4031 <ayyappan4031@gmail.com>
2025-08-23 16:17:37 +0530

    view employee component

---
+++ src/pages/EmployeeManagement/ViewEmployee.jsx
@@ -0,0 +1,30 @@
+import { Descriptions } from "antd";
+import React from "react";
+import dayjs from "dayjs";
+
+const ViewEmployee = ({ employee }) => {
+  return (
+    <>
+      {employee && (
+        <Descriptions bordered column={1} size="small" layout="horizontal">
+          <Descriptions.Item label="Name">{employee.name}</Descriptions.Item>
+          <Descriptions.Item label="Email">{employee.email}</Descriptions.Item>
+          <Descriptions.Item label="Joining Date">
+            {employee.joiningDate
+              ? dayjs(employee.joiningDate).format("DD-MM-YYYY")
+              : "-"}
+          </Descriptions.Item>
+          <Descriptions.Item label="Department">
+            {employee.department}
+          </Descriptions.Item>
+          <Descriptions.Item label="Role">{employee.role}</Descriptions.Item>
+          <Descriptions.Item label="Status">
+            {employee.status}
+          </Descriptions.Item>
+        </Descriptions>
+      )}
+    </>
+  );
+};
+
+export default ViewEmployee;
