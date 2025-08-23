88960694be4da3c05c4acfe9ab99a85e595d20e8
ayyappan4031 <ayyappan4031@gmail.com>
2025-08-23 06:59:17 +0530

    initial commit

---
+++ src/pages/EmployeeManagement/EmployeesTable.jsx
@@ -0,0 +1,12 @@
+import { Table } from "antd";
+import React from "react";
+
+const EmployeesTable = () => {
+  return (
+    <>
+      <Table />
+    </>
+  );
+};
+
+export default EmployeesTable;
---
+++ src/ui/Layout.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import EmployeesTable from "../pages/EmployeeManagement/EmployeesTable";
+const { Header, Content, Footer, Sider } = Layout;
+const siderStyle = {
+  overflow: "auto",
+  height: "100vh",
+  position: "sticky",
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  scrollbarWidth: "thin",
+  scrollbarGutter: "stable",
+};
+const items = [
+  {
+    key: "home",
+    icon: <UserOutlined />,
+    label: "Home",
+  },
+];
+
+const AppLayout = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout hasSider>
+      <Sider style={siderStyle}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            style={{
+              padding: 24,
+              textAlign: "center",
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <EmployeesTable />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Mini-HRMS ©{new Date().getFullYear()} Created by Hfactor
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default AppLayout;
